ea6f2fd94fa354f03b015a966336181e03a41a58
Thalles <thallesemail@mail.com>
2026-02-13 20:54:00 -0300

    Feat: support multiple appliances with list and dictionary storage

--- main.py
+++ main.py
@@ -25,11 +25,45 @@ def calculate_energy():
     print(f"Daily consumption: {daily_consumption:.2f} kWh")
     print(f"Monthly consumption: {monthly_consumption:.2f} kWh")
     print(f"Estimated monthly cost: R${monthly_cost:.2f} (The price is in Brazilian Reais because the origin of this project is within Brazilian territory.)")
+    print("\n-----------------------------------")
+
+    return appliance_name, monthly_consumption, monthly_cost
+
 
 def main():
-    calculate_energy()
+    appliances = []  # Made List
+
+    # PRIMEIRA EXECUÇÃO
+    name, consumption, cost = calculate_energy()
+
+    appliances.append({
+        "appliance_name": name,
+        "monthly_consumption": consumption,
+        "monthly_cost": cost
+    })
+
+    multiple_appliances = input("\nDo you want to add another appliance? (y/n)")
+
+    while multiple_appliances == "y":
+        name, consumption, cost = calculate_energy()
+
+        appliances.append({
+            "appliance_name": name,
+            "monthly_consumption": consumption,
+            "monthly_cost": cost
+        })
+
+        multiple_appliances = input("\nDo you want to add another appliance? (y/n)")
+
+    # Total final
+    total_consumption = sum(a["monthly_consumption"] for a in appliances)
+    total_cost = sum(a["monthly_cost"] for a in appliances)
+
+    print("\n===== HOUSE TOTAL =====")
+    print(f"Total Monthly Consumption: {total_consumption:.2f} kWh")
+    print(f"Total Estimated Cost: R${total_cost:.2f}")
+    print("========================")
+
 
 if __name__ == "__main__":
     main()
-
-
